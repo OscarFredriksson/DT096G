9b0ce4b346e65dc0897bf0ab4b0a1c655c4477c9
Oscar Fredriksson <osfr1701@student.miun.se>
2020-02-02 17:55:55 +0100

    Most tokens are parsed

--- Labb_1/token.h
+++ Labb_1/token.h
@@ -29,32 +29,23 @@ public:
 
         switch(type)
         {
-            case TokenType::STRING:          
-                std::cout << str;
-                break;
-            case TokenType::OR:              
-                std::cout << "+";
-                break;
-            case TokenType::ANY:             
-                std::cout << "*";
-                break;
-            case TokenType::LEFT_PAREN:      
-                std::cout << "(";
-                break;
-            case TokenType::RIGHT_PAREN:     
-                std::cout << ")";
-                break;
-            case TokenType::LEFT_CURLY_BRACKET:
-                std::cout << "{";
-                break;
-            case TokenType::RIGHT_CURLY_BRACKET:
-                std::cout << "}";
-                break;
-            case TokenType::DOT:
-                std::cout << ".";
-                break;
-            default:            
-                break;
+            case TokenType::STRING:                 std::cout << str;
+                                                    break;
+            case TokenType::OR:                     std::cout << "+";
+                                                    break;
+            case TokenType::ANY:                    std::cout << "*";
+                                                    break;
+            case TokenType::LEFT_PAREN:             std::cout << "(";
+                                                    break;
+            case TokenType::RIGHT_PAREN:            std::cout << ")";
+                                                    break;
+            case TokenType::LEFT_CURLY_BRACKET:     std::cout << "{";
+                                                    break;
+            case TokenType::RIGHT_CURLY_BRACKET:    std::cout << "}";
+                                                    break;
+            case TokenType::DOT:                    std::cout << ".";
+                                                    break;
+            default:                                break;
         }
     }
 
